28668ed3218d55b27a0a7f71973123c975db9c96
JellyBeanXiewh <49615695+JellyBeanXiewh@users.noreply.github.com>
2025-03-22 01:03:35 +0800

    feat: 移除axios, pinia, vue-router

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
-import { createPinia } from 'pinia'
-import router from './router/index'
 import Toast from 'vue-toastification'
 import 'vue-toastification/dist/index.css'
 
-const pinia = createPinia()
 const app = createApp(App)
 
-app.use(router)
 app.mount('#app')
-app.use(pinia)
 app.use(Toast)
